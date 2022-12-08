744e684fd8a2d8af5e3e9f3c14e40c80174ba9ca
MongoDB AWS Integrations <101570105+Babusrinivasan76@users.noreply.github.com>
2022-12-08 21:59:58 +0530

    Update pyspark_atlastos3.py

--- code/pyspark_atlastos3.py
+++ code/pyspark_atlastos3.py
@@ -31,8 +31,8 @@ job.init(args['JOB_NAME'], args)
 ## setup the MongoDB Credentials : update the Secret Name and Region ###
 def get_secret():
 
-    secret_name = "<<SECRET_NAME>>" 
-    region_name = "<<REGION_NAME>>"
+    secret_name = $SECRET_NAME
+    region_name = $REGION_NAME
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
@@ -50,25 +50,8 @@ def get_secret():
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
+        if e.response['Error']['Code'] == 'DecryptionFailureException' or 'InternalServiceErrorException' or 'InvalidParameterException' or 'InvalidRequestException' or ''ResourceNotFoundException':
+            print(" Secrets Manager can't decrypt the protected secret text using the provided KMS key")
             raise e
     else:
         # Decrypts secret using the associated KMS key.
@@ -82,8 +65,8 @@ def get_secret():
             return decoded_binary_secret
             
 
-BUCKET_NAME='<<Bucket-Name>>'
-PREFIX='<<Prefix>>'
+BUCKET_NAME=$BUCKET_NAME
+PREFIX=$PREFIX
 
 
  user_name, password, server_addr = get_secret()
@@ -94,8 +77,8 @@ uri = "mongodb+srv://{}.mongodb.net/?retryWrites=true&w=majority'".format(server
 
 read_mongo_options = {
     "uri": uri,
-    "database": "<<Database_name>>",    ## UPDATE the Database name ##
-    "collection": "<<Collection_name>>",   ## UPDATE the Collection name ##
+    "database":$DATABASE_NAME,    
+    "collection": $COLLECTION_NAME,   
     "username": user_name,   
     "password": password  
 }
@@ -111,22 +94,3 @@ glueContext.write_dynamic_frame.from_options(ds, connection_type = "s3", connect
 
 logger.info("written to S3!")
 
-#Renaming created file
-import boto3
-
-
-conn = boto3.client('s3')  # again assumes boto.cfg setup, assume AWS S3
-data = conn.list_objects(Bucket=BUCKET_NAME, Prefix=PREFIX, Delimiter='/')
-
-#Loop in S3 bucket to find the right object
-for objects in data['Contents']:
-    print(objects['Key'])
-    if objects['Key'].startswith(PREFIX + 'run-unnamed'):
-        print('Key', objects['Key'])
-        s3_resource = boto3.resource('s3')
-        copy_source = {
-            'Bucket': BUCKET_NAME,
-            'Key': objects['Key']
-        }
-        s3_resource.Object(BUCKET_NAME, '<<NEW_PREFIX>>/<<NEW_FILENAME>>.json').copy(copy_source)
-        conn.delete_object(Bucket=BUCKET_NAME, Key=objects['Key'])
