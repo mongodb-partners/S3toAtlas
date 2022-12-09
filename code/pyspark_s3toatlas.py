7f8c9c2eafde33dffb6e65d5368e97a231591552
utsavMongoDB <114057324+utsavMongoDB@users.noreply.github.com>
2022-12-09 15:45:09 +0530

    Update pyspark_s3toatlas.py
    Updated with latest code

--- code/pyspark_s3toatlas.py
+++ code/pyspark_s3toatlas.py
@@ -18,7 +18,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 ## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'SECRET_NAME', 'REGION_NAME', 'BUCKET_NAME', 'DATABASE_NAME', 'COLLECTION_NAME'])
 
 sc = pyspark.SparkContext()
 glueContext = GlueContext(sc)
@@ -31,8 +31,8 @@ job.init(args['JOB_NAME'], args)
 ## setup the MongoDB Credentials ###
 def get_secret():
 
-    secret_name = $SECRET_NAME
-    region_name = $REGION_NAME
+    secret_name = args['SECRET_NAME']
+    region_name = args['REGION_NAME']
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
@@ -62,15 +62,14 @@ def get_secret():
             decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
             return decoded_binary_secret
             
-
 user_name, password, server_addr = get_secret()
 
 ## Read from the S3 bucket ###.  ## UPDATE the S3 bucket name ##
 
-ds = glueContext.create_dynamic_frame_from_options(connection_type = "s3", connection_options={"paths": ["s3://{}/airports.json".format($BUCKET_NAME)]}, format="json", format_options={}, transformation_ctx = "")
+ds = glueContext.create_dynamic_frame_from_options(connection_type = "s3", connection_options={"paths": ["s3://{}/airports.json".format(args['BUCKET_NAME'])]}, format="json", format_options={}, transformation_ctx = "")
 
 #### MongoDB Atlas Connection ###. 
- mongo_uri = "mongodb+srv://{}.mongodb.net/?retryWrites=true&w=majority'".format(server_addr) 
+mongo_uri = "mongodb+srv://{}.mongodb.net/?retryWrites=true&w=majority".format(server_addr) 
 
 
 logger = glueContext.get_logger()
@@ -78,9 +77,9 @@ logger.info("Connecting...")
 
 write_mongo_options = {
     "uri": mongo_uri,
-    "database": $DATABASE_NAME,
-    "collection": $COLLECTION_NAME,
-    "username": username,   
+    "database": args['DATABASE_NAME'],
+    "collection":args['COLLECTION_NAME'],
+    "username": user_name,   
     "password": password  
 }
 
